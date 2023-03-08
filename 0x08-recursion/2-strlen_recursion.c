6ecdc43f123d131dac9c5b3cb2bf2e28639ec4be
igapeterclavors <iga.peterclavors@gmail.com>
2023-03-08 12:05:55 -0800

    recussion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int slength = 0;
+
+	if (*s)
+	{
+		slength++;
+		slength += _strlen_recursion(s + 1);
+	}
+
+	return (slength);
+}
+
