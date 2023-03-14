5d0be6c246959cc4a8d62dfba47f47cc5e4ca4e1
igapeterclavors <iga.peterclavors@gmail.com>
2023-03-14 13:37:26 -0700

    all tasks

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * _strdup - duplicate to new memory space location
  * @str: char
+ *
  * Return: 0
  */
 char *_strdup(char *str)
@@ -21,5 +23,4 @@ char *_strdup(char *str)
 	for (r = 0; str[r]; r++)
 		a[r] = str[r];
 	return (a);
-
 }
